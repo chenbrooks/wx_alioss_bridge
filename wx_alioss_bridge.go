fe7012b2336258569f163d886595975761135f51
chenbrooks <baiysset@gmail.com>
2017-03-15 00:44:34 +0800

    增加对本地文件的支持
    如果把组件部署到本地，也可以上传阿里云服务器

--- wx_alioss_bridge.go
+++ wx_alioss_bridge.go
@@ -28,7 +28,9 @@ func main() {
  */
 func webServer() {
 	fmt.Println("start server")
-	http.HandleFunc("/aliyun", aliyunOss)
+	http.HandleFunc("/aliyun/wx", wx2Oss)
+	http.HandleFunc("/aliyun/xiumi", xiumi2Oss)
+	http.HandleFunc("/aliyun/local", local2Oss)
 
 	err := http.ListenAndServe("127.0.0.1:9527", nil);
 	if err != nil{
@@ -36,7 +38,55 @@ func webServer() {
 	}
 }
 
-func aliyunOss(writer http.ResponseWriter, request *http.Request) {
+func local2Oss(writer http.ResponseWriter, request *http.Request){
+
+	request.ParseForm()
+	param_imageDir, found1 := request.Form["imagedir"]
+	param_bucketName, found3 := request.Form["bucketname"];
+	param_accessKeyID, found4 := request.Form["keyid"];
+	param_accessKeySecret, found5 := request.Form["keysecret"];
+	if !found1 {
+		fmt.Fprint(writer, "please provide image info: image dir")
+		return
+	}
+
+	if !(found3 && found4 && found5){
+		fmt.Fprint(writer, "please provide oss info: bucketname or keyid or key")
+		return
+	}
+	filename := downloadLocalImage(param_imageDir[0])
+
+	fmt.Println(filename);
+	fullAddress := uploadAliyunImage(filename, param_bucketName[0], param_accessKeyID[0], param_accessKeySecret[0] )
+	fmt.Print( fullAddress)
+	fmt.Fprint(writer, fullAddress)
+}
+
+
+func xiumi2Oss(writer http.ResponseWriter, request *http.Request){
+	request.ParseForm()
+	param_imageURL, found1 := request.Form["imageurl"]
+	param_bucketName, found3 := request.Form["bucketname"];
+	param_accessKeyID, found4 := request.Form["keyid"];
+	param_accessKeySecret, found5 := request.Form["keysecret"];
+	if !found1 {
+		fmt.Fprint(writer, "please provide image info: image url")
+		return
+	}
+
+	if !(found3 && found4 && found5){
+		fmt.Fprint(writer, "please provide oss info: bucketname or keyid or key")
+		return
+	}
+	filename := downloadXiuMiImage(param_imageURL[0])
+
+	fmt.Println(filename);
+	fullAddress := uploadAliyunImage(filename, param_bucketName[0], param_accessKeyID[0], param_accessKeySecret[0] )
+	fmt.Print( fullAddress)
+	fmt.Fprint(writer, fullAddress)
+}
+
+func wx2Oss(writer http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
 	param_accessToken, found1 := request.Form["accesstoken"]
 	param_imageID, found2:= request.Form["imageid"]
@@ -65,14 +115,45 @@ func aliyunOss(writer http.ResponseWriter, request *http.Request) {
 }
 
 
+
+
+
 // http://idcars-test.oss-cn-shenzhen.aliyuncs.com/
 func getFullUrl(filename string, bucketname string) string {
 	return "http://" + bucketname + "."+oss_domain+ "/" + filename;
 }
 
+
 func downloadWXImage(accessToken string, wxImageID string) string {
 	rawURL := "https://api.weixin.qq.com/cgi-bin/media/get?access_token="
 	url := rawURL + accessToken + "&media_id=" + wxImageID
+	return downloadFile(url);
+}
+
+func downloadXiuMiImage(imageUrl string) string{
+	return downloadFile(imageUrl)
+}
+
+func downloadLocalImage(imageDir string) string{
+
+	filename := getFileName()
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	src, _:= os.Open(imageDir)
+	_, err = io.Copy(file,	src)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	file.Close()
+	fmt.Println("download success")
+	return filename;
+}
+
+func downloadFile(url string) string  {
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -95,10 +176,14 @@ func downloadWXImage(accessToken string, wxImageID string) string {
 	return filename;
 }
 
+
 func uploadAliyunImage(filename string, bucketName string, keyID string, keySecret string) string {
 	aliyunFile := strings.Replace(filename, "/tmp/", "", 1)
-	//client, err := oss.New(oss_domain, "LTAIZ9aDNO2SCUex", "48vj1cOVe1RRBTxgNiEMujZI3mSE35")
-	client, err := oss.New(oss_domain, keyID, keySecret)
+	//if(keyID == ""){
+	client, err := oss.New(oss_domain, "LTAIZ9aDNO2SCUex", "48vj1cOVe1RRBTxgNiEMujZI3mSE35")
+	//}
+
+	//client, err := oss.New(oss_domain, keyID, keySecret)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -111,7 +196,7 @@ func uploadAliyunImage(filename string, bucketName string, keyID string, keySecr
 		log.Fatal(err)
 	}
 	fmt.Println("upload success")
-	return aliyunFile
+	return getFullUrl(aliyunFile, bucketName)
 }
 
 func getFileName() string {
